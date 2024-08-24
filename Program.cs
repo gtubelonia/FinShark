8b24b5512b147a5aed3bcb36faaafe72c5550fff
Gian T <Gtubelonia@live.com>
2024-08-23 19:27:07 -0500

    added Stock API/Controller can pull single or all stock

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using FinShark.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Add SqlConnection
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
