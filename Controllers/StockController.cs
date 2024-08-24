d07ac5dafa4352a080f5b918bf379293912a4316
Gian T <Gtubelonia@live.com>
2024-08-24 09:24:51 -0500

    Added new mappers and synchronous Post, Get, Update, and Delete commands to StockApi

---
+++ Controllers/StockController.cs
@@ -0,0 +1,89 @@
+﻿using FinShark.Data;
+using FinShark.Dtos.Stock;
+using FinShark.Mappers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinShark.Controllers
+{
+    [ApiController]
+    [Route("api/stock")]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+        public StockController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            //Deferred execution.
+            var stocks = _context.Stocks.ToList()
+                .Select(s => s.ToStockDto());
+            return Ok(stocks);
+        }
+        //extract ID from url
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            //Can use first or default too
+            var stock = _context.Stocks.Find(id);
+
+            if (stock == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(stock.ToStockDto());
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] CreateStockRequestDto stockDto)
+        {
+            var stockModel = stockDto.ToStockFromCreateDto();
+            _context.Stocks.Add(stockModel);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+            if (stockModel == null)
+            {
+                return NotFound();
+            }
+
+            stockModel.Symbol = updateDto.Symbol;
+            stockModel.CompanyName = updateDto.CompanyName;
+            stockModel.Purchase = updateDto.Purchase;
+            stockModel.LastDiv = updateDto.LastDiv;
+            stockModel.Industry = updateDto.Industry;
+            stockModel.MarketCap = updateDto.MarketCap;
+
+            _context.SaveChanges();
+
+            return Ok(stockModel.ToStockDto());
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+
+            if (stockModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.Stocks.Remove(stockModel);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}
---
+++ Mappers/StockMappers.cs
@@ -0,0 +1,36 @@
+﻿using FinShark.Dtos.Stock;
+using FinShark.Models;
+using System.Runtime.CompilerServices;
+
+namespace FinShark.Mappers
+{
+    public static class StockMappers
+    {
+        public static StockDto ToStockDto(this Stock stockModel)
+        {
+            return new StockDto
+            {
+                Id = stockModel.Id,
+                Symbol = stockModel.Symbol,
+                CompanyName = stockModel.CompanyName,
+                Purchase = stockModel.Purchase,
+                LastDiv = stockModel.LastDiv,
+                Industry = stockModel.Industry,
+                MarketCap = stockModel.MarketCap
+            };
+        }
+
+        public static Stock ToStockFromCreateDto(this CreateStockRequestDto CreateStockRequestDto)
+        {
+            return new Stock
+            {
+                Symbol = CreateStockRequestDto.Symbol,
+                CompanyName = CreateStockRequestDto.CompanyName,
+                Purchase = CreateStockRequestDto.Purchase,
+                LastDiv = CreateStockRequestDto.LastDiv,
+                Industry = CreateStockRequestDto.Industry,
+                MarketCap = CreateStockRequestDto.MarketCap
+            };
+        }
+    }
+}
