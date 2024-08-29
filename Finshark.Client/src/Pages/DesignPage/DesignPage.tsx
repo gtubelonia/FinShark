780a0e4da78c3c4c8be467c7cba82e7b9a14f396
Gian T <Gtubelonia@live.com>
2024-08-29 10:49:39 -0500

    Added a RatioList component and implemented in the design page

--- Finshark.Client/src/Pages/DesignPage/DesignPage.tsx
+++ Finshark.Client/src/Pages/DesignPage/DesignPage.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Table from '../../Components/Table/Table'
+import RatioList from '../../Components/RatioList/RatioList'
 
 type Props = {}
 
 const DesignPage = (props: Props) => {
   return (
     <>
-    <h1>FinShark Design Page</h1>
-    <h2>This is FinShark's design page. This is where we house various design aspects of the app</h2>
-    <Table />
+      <h1>FinShark Design Page</h1>
+      <h2>This is FinShark's design page. This is where we house various design aspects of the app</h2>
+      <RatioList />
+      <Table />
     </>
 
   )
