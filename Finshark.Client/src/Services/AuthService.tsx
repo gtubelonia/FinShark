7537b6a1b21202c849d7e2c363be4b62e13ecd5a
Gian T <Gtubelonia@live.com>
2024-08-29 20:48:24 -0500

    added api call to user login and register.
    not yet implemented on any pges

--- Finshark.Client/src/Services/AuthService.tsx
+++ Finshark.Client/src/Services/AuthService.tsx
@@ -1,1 +1,29 @@
-const api = "http://localhost:"
+import { handleError } from "../Helpers/ErrorHandler";
+import { UserProfileToken } from "../Models/User";
+
+const api = "http://localhost:5097"
+
+export const loginAPI = async (username: string, password: string) => {
+    try {
+        const data = await axios.post<UserProfileToken>(api + "account/login", {
+            username: username,
+            password: password,
+        });
+        return (data);
+    } catch (error) {
+        handleError();
+    }
+}
+
+export const registerAPI = async (username: string, password: string, email: string) => {
+    try {
+        const data = await axios.post<UserProfileToken>(api + "account/register", {
+            username: username,
+            password: password,
+            email: email,
+        });
+        return (data);
+    } catch (error) {
+        handleError();
+    }
+}
