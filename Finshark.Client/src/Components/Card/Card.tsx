f6eba1870f8647281696f1bb7876137b7fc04855
Gian T <Gtubelonia@live.com>
2024-08-30 22:57:19 -0500

    Updated links in stock search results. should be more obvious that they are clickable

--- Finshark.Client/src/Components/Card/Card.tsx
+++ Finshark.Client/src/Components/Card/Card.tsx
@@ -13,16 +13,19 @@ type Props = {
 const Card = ({ id, searchResult, onPortfolioCreate }: Props): JSX.Element => {
     return (
         <div
-            className="flex flex-col items-center justify-between w-full p-6 bg-slate-100 rounded-lg md:flex-row"
+            className="flex flex-col items-center justify-evenly w-full p-6 rounded-lg md:flex-row"
             key={id}
             id={id}
         >
-            <Link to = {`/company/${searchResult.symbol}/company-profile`}
-                className="font-bold text-center text-black md:text-left"
+
+            <Link to={`/company/${searchResult.symbol}/company-profile`}
+                className="font-bold btn text-center text-black md:text-left"
             >
-                {searchResult.name} ({searchResult.symbol})
+                <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+                    {searchResult.name} ({searchResult.symbol})
+                </button>
             </Link>
-            <p className="text-black">{searchResult.currency}</p>
+            <p className="text-black p-2"> {searchResult.currency} </p>
             <p className="font-bold text-black">
                 {searchResult.exchangeShortName} - {searchResult.stockExchange}
             </p>
